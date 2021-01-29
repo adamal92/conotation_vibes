99cce78b1e2cb27ec3dd412bbc184391a19213ee
adam l <livneadam@gmail.com>
2021-01-30 01:21:54 +0200

    firebase setup done

--- server/server.py
+++ server/server.py
@@ -1,26 +1,34 @@
 from textblob import TextBlob
 import os
 
-def recognize(file_string: str, keyword: str):
+
+"""
+This file contains the server of the program.
+"""
+
+
+def recognize(file_string, keyword: str):
     """
-        param file_string: A string that represent a data from file
+        param file_string: A file that represent a data file
         param keyword: A string with a keyword to search
     """
     # Here goes the scraping result for articles .....
-    blob = TextBlob(file_string)
+    feed = file_string.read()
+    blob = TextBlob(feed)
     print(blob.sentiment)
-   
+
 
 def main():
     """
         Main function of the server.
         Includes the calls for conotation recognition functions and calls the data base queries
     """
-    with open(fr'{os.getcwd()}\wiki.txt', "r") as file:
-        recognize(file.read().lower(), "israel")
+    with open(fr'{os.getcwd()}\wiki.txt', "r", encoding='utf-8') as f:
+        recognize(f, "israel")
     # feed = "the food at Ruby's place is awful"
     # blob = TextBlob(feed)
     # print(blob.sentiment)
+
 
 if __name__ == "__main__":
     main()
