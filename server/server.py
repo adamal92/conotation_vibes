1c8ec595bbbccb597ebb27fc8b5a783c99cf2761
adam l <livneadam@gmail.com>
2021-02-02 01:01:21 +0200

    speech recog works

--- server/server.py
+++ server/server.py
@@ -1,76 +1,77 @@
-from io import TextIOWrapper
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+import logging
+import time
 
-from textblob import TextBlob
-import os
+# https://stackoverflow.com/questions/22715086/scheduling-python-script-to-run-every-hour-accurately
+# https://data.gov.il/dataset/covid-19  # מאגר קורונה
+# https://data.gov.il/dataset/covid-19/resource/8a21d39d-91e3-40db-aca1-f73f7ab1df69  # טבלת יישובים
+# https://data.gov.il/dataset/covid-19/resource/0995c344-6a7a-4557-99ff-28ee6f3149b3  # טבלת יישובים README
+# https://data.gov.il/dataset/covid-19/resource/89f61e3a-4866-4bbf-bcc1-9734e5fee58e  # קבוצות מין וגיל
+# https://console.firebase.google.com/u/2/project/corona-charts-33e8a/database/corona-charts-33e8a-default-rtdb/data/~2F
+# https://stackoverflow.com/questions/30483977/python-get-yesterdays-date-as-a-string-in-yyyy-mm-dd-format/30484112
 
-import nltk
+from playsound import playsound
+import sys
 
-from data_types_and_structures import DataTypesHandler
-
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('brown')
-from nltk.stem import LancasterStemmer
-"""
-This file contains the server of the program.
-"""
+sys.path.append(".")
+from serverLib import *
 
 
-def recognize(file_string: TextIOWrapper, keyword: str) -> dict:
-    """
-        param file_string: A file that represent a data file
-        param keyword: A string with a keyword to search
-    """
-    # Here goes the scraping result for articles .....
-    feed = file_string.read()
-    blob = TextBlob(feed)
-    # print(blob.sentiment)
-    # print(blob.tags)
-    # print(blob.noun_phrases)
-    # print(blob.sentiment.polarity)
-    # print(blob.words)
-    # print(blob.sentences)
-    lst = LancasterStemmer()
-    ret_dict = {}
-    counter = 0
-    ret_dict["connotation_list"] = []
-    for sentence in blob.sentences:
-        # print(sentence)
-        for word in sentence.words:
-            # print(lst.stem(word))
-            if lst.stem(word) == lst.stem(keyword):
-                counter += 1
-                ret_dict["connotation_list"].append(
-                    {
-                        "polarity": sentence.sentiment.polarity,
-                        "subjectivity": sentence.sentiment.subjectivity,
-                        "sentence": sentence.correct()
-                    }
-                )
-                # print(sentence.sentiment)
-    ret_dict["times_found"] = counter
-    ret_dict["total_connotation"] = {
-        "polarity": blob.sentiment.polarity,
-        "subjectivity": blob.sentiment.subjectivity
-    }
-    return ret_dict
+def start_server():
+    run_py_file("sentiment_text")
+
+    # run_py_file("red_green_countries")  # red green
+    # run_py_file("load_latest_json_5_cities_israel_redGreen")  # israel_final, cities_final
+    # run_py_file("load_country")  # israel_UN_WHO, WorldWide_stats
+    # run_py_file("load_graph_2")  # graph_3
+
+    # crawl_corona_UN()
+    # crawl_corona_red_green()
+    # crawl_corona_israel_final()
+
+
+@Constants.SCHEDULER.scheduled_job('cron', day_of_week='mon-sun', hour=7, minute=0, second=0)
+def scheduled_job():
+    print('This job is run every weekday at 7am.')
+    firebase_config()
+    start_server()
+    print(datetime.now())
+    playsound(
+        "/home/kobi/Adam_desk/BigData/BigData-master/BD_projects/Ruby_corona_charts/linux server/python/arrow_fx.wav")
+    logging.debug(f"Program Total Time: {time.time() - st} seconds")
+    logging.debug(f"Program Total Time: {(time.time() - st)//60} minutes")
 
 
 def main():
-    """
-        Main function of the server.
-        Includes the calls for conotation recognition functions and calls the data base queries
-    """
+    RUN_SCHEDULER = False  # True
+    # kobi@kobi-A1SAi:~/Adam_desk/BigData/BigData-master/BD_projects$ python3 Ruby_corona_charts/python/linuxServer.py
+    st = time.time()
 
-    with open(fr'{os.getcwd()}\wiki.txt', "r", encoding='utf-8') as f:
-        # recognize(f, "israel")
-        DataTypesHandler.print_data_recursively(
-            data=recognize(f, "israel"), print_dict=DataTypesHandler.PRINT_DICT
-        )
-    # feed = "the food at Ruby's place is awful"
-    # blob = TextBlob(feed)
-    # print(blob.sentiment)
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('flaskwebgui').setLevel(logging.ERROR)
+    logging.getLogger('BaseHTTPRequestHandler').setLevel(logging.ERROR)
+    logging.getLogger('matplotlib').setLevel(logging.ERROR)
+    logging.getLogger('py4j').setLevel(logging.ERROR)
+    logging.getLogger('my_log').setLevel(logging.DEBUG)
+
+    try:
+        firebase_config()
+        print(datetime.now())
+
+        if RUN_SCHEDULER:
+            # pass
+            start_server()
+            Constants.SCHEDULER.start()
+        else:
+            start_server()
+    finally:
+        from playsound import playsound
+        playsound("/home/kobi/Adam_desk/BigData/BigData-master/BD_projects/Ruby_corona_charts/linux server/python/arrow_fx.wav")
+        # playsound("arrow_fx.wav")
+        logging.debug(f"Program Total Time: {time.time() - st} seconds")
+        logging.debug(f"Program Total Time: {(time.time() - st)//60} minutes")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- server/serverLib.py
+++ server/serverLib.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import logging
 import os
 import subprocess
@@ -6,11 +7,13 @@ import time
 from subprocess import Popen
 from typing import List, Dict
 
-import requests
+from firebase import Firebase
+# pip install python_jwt
+# pip install gcloud
+# pip install sseclient
+# pip install pycrypto
+# pip install requests-toolbelt
 from apscheduler.schedulers.blocking import BlockingScheduler
-from pyspark import Row, Accumulator
-from pyspark.python.pyspark.shell import spark
-from pyspark.sql import DataFrame
 
 
 class Constants:
@@ -45,166 +48,20 @@ def run_py_file(filename: str):
     process.communicate()  # wait for process to end
 
 
-def crawl_corona_UN():
-    response = requests.get(url=Constants.API_URL)
-    print(response.text)
-    from testsAndOthers.data_types_and_structures import DataTypesHandler
-    DataTypesHandler.print_data_recursively(data=response.json(), print_dict=DataTypesHandler.PRINT_DICT)
-    print(response.json()["features"][0]["attributes"])
-    Constants.db.update(
-        {
-            "israel_UN_WHO": response.json()["features"][0]["attributes"]
-        }
-    )
-    return
+def firebase_config():
+    config = {
+        # "apiKey": "apiKey",
+        # "authDomain": "projectId.firebaseapp.com",
+        # "databaseURL": "https://databaseName.firebaseio.com",
+        # "storageBucket": "projectId.appspot.com",
+        # "serviceAccount": "path/to/serviceAccountCredentials.json"  # (optional)
 
+        "apiKey": "AIzaSyCOt619fNqEuIgFpzf20h2cmC6tFeQYuTE",
+        "authDomain": "corona-charts-33e8a.firebaseapp.com",
+        "databaseURL": "https://corona-charts-33e8a-default-rtdb.firebaseio.com/",
+        "storageBucket": "corona-charts-33e8a.appspot.com"
+    }
 
-def crawl_corona_red_green():
-    # TODO: catch if there is no internet connection
-    response: dict = requests.get(Constants.URL_GREEN_RED).json()
-    to_spark_direct_upside_down_red_green(countries=response["result"]["records"])
+    firebase = Firebase(config)
 
-
-def to_spark_direct_upside_down_red_green(countries: dict):
-    countiesDF: DataFrame = spark.createDataFrame(data=countries)
-    countiesDF.cache()  # without: 152.403 s, with: 14.531 s
-
-    from datetime import datetime, timedelta
-    result_length: int = 0
-    day: datetime = datetime.now()  # today
-    while result_length == 0:
-        day_str: str = datetime.strftime(day, '%Y-%m-%d')
-        filteredDF: DataFrame = countiesDF.filter(countiesDF.update_date >= day_str)
-        filteredDF.cache()
-
-        schema = filteredDF.columns
-
-        final_result: List[Dict] = filteredDF.collect()
-
-        logging.debug(f'{day_str} {type(day_str)}')
-        logging.debug("Empty RDD: %s" % (final_result.__len__() == 0))
-        day = day - timedelta(1)  # timedelta() indicates how many days ago
-        result_length = final_result.__len__()
-
-    logging.debug(schema)
-
-    Constants.db.update(
-         {
-             "Countries_Red_Green": filteredDF.toPandas().to_dict()
-         }
-    )
-
-    def append_json(row: Row[tuple]):
-        return {row["destination"]: row.country_status}  # {"counter": row.asDict()}
-
-    filteredDF = filteredDF.select("destination", "country_status")
-    filteredDF.show()
-    cities_final_df: DataFrame = spark.createDataFrame(data=filteredDF.rdd.map(append_json).collect())
-
-    cities_final_df.cache()
-
-    dicty = {}
-    for row in filteredDF.collect():
-        dicty[row.destination] = row.country_status
-
-    from testsAndOthers.data_types_and_structures import DataTypesHandler
-    DataTypesHandler.print_data_recursively(
-        data=dicty, print_dict=DataTypesHandler.PRINT_DICT
-    )
-
-    Constants.db.update(
-        {
-            "Countries_Red_Green_2": {
-                "full_table": filteredDF.toPandas().to_dict(),
-                "collected": filteredDF.collect(),
-                # "dicty": dicty
-                # "color_by_country": cities_final_df.toPandas().to_dict()
-            }
-        }
-    )
-
-
-def crawl_corona_israel_final():
-    response: dict = requests.get(Constants.URL_GOV).json()
-    to_spark_direct_upside_down_israel_final(cities=response["result"]["records"])
-
-
-def to_spark_direct_upside_down_israel_final(cities: dict):
-    st = time.time()
-
-    citiesDF: DataFrame = spark.createDataFrame(data=cities)
-    citiesDF.cache()
-
-    from datetime import datetime, timedelta
-    result_length: int = 0
-    day: datetime = datetime.now()  # today
-    while result_length == 0:
-        day_str: str = datetime.strftime(day, '%Y-%m-%d')
-        filteresDF: DataFrame = citiesDF.filter(citiesDF.Date >= day_str)
-
-        schema = filteresDF.columns
-
-        final_result: List[Dict] = filteresDF.collect()
-
-        logging.debug(f'{day_str} {type(day_str)}')
-        logging.debug("Empty RDD: %s" % (final_result.__len__() == 0))
-        day = day - timedelta(1)  # timedelta() indicates how many days ago
-        result_length = final_result.__len__()
-
-    del citiesDF
-    filteresDF.cache()
-
-    def append_json(row: Row):
-        return {row["City_Name"]: row.asDict()}  # {"counter": row.asDict()}
-
-    filteresDF.foreach(append_json)
-    cities_final_df: DataFrame = spark.createDataFrame(data=filteresDF.rdd.map(append_json).collect())
-    # cities_final_df.cache()
-
-    Constants.db.update(
-        {"cities_final": cities_final_df.toPandas().to_dict()}
-    )  # load to firebase
-
-    total: Accumulator = spark.sparkContext.accumulator(0)
-    less: Accumulator = spark.sparkContext.accumulator(0)
-    keys_lst: list = []
-    updated_to: str = ''
-
-    def add(row: Row, total: Accumulator, less: Accumulator):
-        for val in row.asDict().values():
-            if str(val).isdigit():
-                total += int(val)
-            elif val == "<15":
-                less += 1
-
-    for key in filteresDF.toPandas().keys():
-        if key == 'Date':
-            print(filteresDF.rdd.first().Date)
-            updated_to = filteresDF.rdd.first().Date
-            continue
-        elif key in ["_id", "City_Name", "City_Code"]: continue
-        print(key)
-        filteresDF.select(filteresDF[key]).foreach(lambda row: add(row, total, less))
-        print(total.value)
-        print(less.value)
-        keys_lst.append(
-            {
-                str(key): {
-                    "total": total.value,
-                    "less_<15": less.value
-                }
-            }
-        )
-        total.value = 0
-        less.value = 0
-
-    Constants.db.update(
-        {
-            "israel_final": {
-                "data": keys_lst,
-                "Last_Update": updated_to
-            }
-        }
-    )
-
-    logging.debug(f"spark total time: {time.time() - st} seconds")
+    Constants.db = firebase.database()
